3c72a03bc2feb7f28eec913a28104f5d9c5c0e1f
natebwilson <natebwilson@gmail.com>
2026-03-16 12:14:42 -0400

    Updated ALU.v, expanded ALUControl, added new operators

--- LA3/ALU.v
+++ LA3/ALU.v
@@ -1,7 +1,7 @@
 module ALU(
 	input [31:0] A,
 	input [31:0] B,
-	input [2:0] ALUcontrol,
+	input [3:0] ALUcontrol,
 	output reg [31:0] result,
 	output [3:0] flags);
 	
@@ -10,7 +10,7 @@ module ALU(
 	wire cout;
 	
 	wire Condition1;
-	assign Condition1 = (~ALUcontrol[2] & ~ALUcontrol[1]) | (~ALUcontrol[1] & ALUcontrol[0]);
+	assign Condition1 = (~ALUControl[3] & ~ALUcontrol[2] & ~ALUcontrol[1]) | (~ALUControl[3] & ~ALUcontrol[1] & ALUcontrol[0]);
 	
 	wire Condition2;
 	assign Condition2 = (A[31] ^ sum[31]);
@@ -28,12 +28,16 @@ module ALU(
 	
 	always @(ALUcontrol, sum, A, B, v, c, cout)
 		case (ALUcontrol)
-			3'b000: result <= sum;
-			3'b001: result <= sum;
-			3'b010: result <= A & B;
-			3'b011: result <= A | B;
-			3'b101: result <= {31'b0, (sum[31] ^v)};
-			3'b110: result <= A << B[4:0];
+			4'b0000: result <= sum;
+			4'b0001: result <= sum;
+			4'b0010: result <= A & B;
+			4'b0011: result <= A | B;
+			4'b0100: result <= A ^ B;
+			4'b0101: result <= {31'b0, (sum[31] ^v)};
+			4'b0110: result <= A << B[4:0];
+			4'b0111: result <= A >> B[4:0];
+			4'b1000: result <= $signed(A) >>> B[4:0];
+			4'b1001: result <= {31'b0, ~cout}; //cout is passively calculated
 			default: result <= 32'bx;
 		endcase
 endmodule
